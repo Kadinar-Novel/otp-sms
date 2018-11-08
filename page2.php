7033bfe9cdc3cfe8a0bf309698d89bf3c0dbcf23
Kadinar Novel <32504212+Kadinar-Novel@users.noreply.github.com>
2018-11-08 11:33:08 +0700

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,7 @@
+<?php
+    if(!isset($_COOKIE['OTP'])) {
+        echo "Cookie is not set";
+    } else {
+        echo 'Welcome to mobile legends, Five Seconds til the enemy reach the battlefiled, smash them. All Troops deployed.';
+    }
+?>
